0e42d1b0f93a92f06d92c90fe6a5ed22aeb35898
Alexander-Ryzhkov <alexander.ryzhkov.96@gmail.com>
2015-10-05 18:41:45 +0300

    Main task added
    May be not so good data displaying

--- Числ.мет.ЛР4.m
+++ Числ.мет.ЛР4.m
@@ -1,11 +1,12 @@
-"testing git"
-pi=SetPrecision[3.141592,8];
+(* ::Package:: *)
+
+pi=SetPrecision[3.141593,8]
 
 
 x0={0,pi/6,pi/4,pi/3,pi/2}
 f0={0,Sin[2pi/6],Sin[2pi/4],Sin[2pi/3],Sin[2pi/2]}
 L=Length[x0]
-p={pi/12,5pi/12,7pi/24}
+p={pi/12.,5.pi/12,7.pi/24}
 
 
 A[k_]:=1/Product[x0[[k]]-x0[[i]],{i,Complement[Range[L],{k}]}]
@@ -13,10 +14,10 @@ A[k_]:=1/Product[x0[[k]]-x0[[i]],{i,Complement[Range[L],{k}]}]
 P[x_]:=Sum[(A[i] f0[[i]])/(x-x0[[i]]),{i,L}]/Sum[A[i] /(x-x0[[i]]),{i,L}]
 
 
-P/@p
+P0=P/@p
 
 
-Sin/@ (2p)
+Sin0=Sin/@ (2p)
 
 
 Abs[Sin/@ (2p)-P/@p]
@@ -40,29 +41,34 @@ LP[n_,i_,x_]:=1/(x0[[i+n]]-x0[[i]]) Det[({
 
 
 
-LP[4,1,7pi/24]
-
-
-Precision[1.]
+LP[4,1,7pi/24]+0.0
 
 
 LP[4,1,x]
 
 
-Plot[%101,{x,0,7.1pi/24}]
+SetAttributes[LP,Listable]
 
 
-P[x]
+COL[k_]:=LP[k,Range[L-k],x]
 
 
-Simplify[%107]
+S=Col/@Range[4]
 
 
-Plot[%108,{x,0,2}]
+S/.x->p[[1]]
 
 
+S/.x->p[[2]]
 
 
+S/.x->p[[3]]
 
 
-%%Somthing new
+LP[4,1,p]
+
+
+P0
+
+
+Sin0
