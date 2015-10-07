fefda42ffac94382d74bf0fc848a97c4b79e3bc0
Denis <denis.balashevich@gmail.com>
2015-10-07 20:33:59 +0300

    Revert "Main task added"
    This reverts commit 0e42d1b0f93a92f06d92c90fe6a5ed22aeb35898.

--- Числ.мет.ЛР4.m
+++ Числ.мет.ЛР4.m
@@ -1,12 +1,11 @@
-(* ::Package:: *)
-
-pi=SetPrecision[3.141593,8]
+"testing git"
+pi=SetPrecision[3.141592,8];
 
 
 x0={0,pi/6,pi/4,pi/3,pi/2}
 f0={0,Sin[2pi/6],Sin[2pi/4],Sin[2pi/3],Sin[2pi/2]}
 L=Length[x0]
-p={pi/12.,5.pi/12,7.pi/24}
+p={pi/12,5pi/12,7pi/24}
 
 
 A[k_]:=1/Product[x0[[k]]-x0[[i]],{i,Complement[Range[L],{k}]}]
@@ -14,10 +13,10 @@ A[k_]:=1/Product[x0[[k]]-x0[[i]],{i,Complement[Range[L],{k}]}]
 P[x_]:=Sum[(A[i] f0[[i]])/(x-x0[[i]]),{i,L}]/Sum[A[i] /(x-x0[[i]]),{i,L}]
 
 
-P0=P/@p
+P/@p
 
 
-Sin0=Sin/@ (2p)
+Sin/@ (2p)
 
 
 Abs[Sin/@ (2p)-P/@p]
@@ -41,34 +40,29 @@ LP[n_,i_,x_]:=1/(x0[[i+n]]-x0[[i]]) Det[({
 
 
 
-LP[4,1,7pi/24]+0.0
+LP[4,1,7pi/24]
+
+
+Precision[1.]
 
 
 LP[4,1,x]
 
 
-SetAttributes[LP,Listable]
+Plot[%101,{x,0,7.1pi/24}]
 
 
-COL[k_]:=LP[k,Range[L-k],x]
+P[x]
 
 
-S=Col/@Range[4]
+Simplify[%107]
 
 
-S/.x->p[[1]]
+Plot[%108,{x,0,2}]
 
 
-S/.x->p[[2]]
 
 
-S/.x->p[[3]]
 
 
-LP[4,1,p]
-
-
-P0
-
-
-Sin0
+%%Somthing new
