10bf28da0204ca80275dea17ffe8c069e5669780
Alexander-Ryzhkov <alexander.ryzhkov.96@gmail.com>
2015-11-04 22:36:59 +0300

    6.1 completed

--- Данные.m
+++ Данные.m
@@ -1,22 +1,21 @@
 (* ::Package:: *)
 
-ClearAll[pi, x0, f, f0, Tzero, A, P, LP, CNP, NP, SG];
+ClearAll[pi, x0, f, Tzero, A, P, LP, CNP, NP, TSA, TRO];
 
 
 $MinPrecision = 8;
 
 
 (*Initial data*)
-pi=SetPrecision[3.141593,8];
+pi=SetPrecision[3.141593,15];
 x0={0,pi/6,pi/4,pi/3,pi/2};
 f[x_]:=Sin[2x];
 SetAttributes[f,Listable];
-f0=f[x0];
 p={pi/12,5pi/12,7pi/24};
 
 
 (*Chebyshev polynom's zeros*)
-Tzero[n_]:=Table[pi/4 Cos[(2 k-1)/(2*n) pi]+pi/4,{k,n}];
+Tzero[n_]:=Table[pi/4 * Cos[pi * (2*k - 1)/(2*n)]+pi/4,{k,n}];
 
 
 (*Barycentric form of Lagrange polynom*)
@@ -26,8 +25,8 @@ P[x_,x0_]:=Sum[(A[i,x0] f0[[i]])/(x-x0[[i]]),{i,Length[x0]}]/Sum[A[i,x0] /(x-x0[
 
 (*Aitkin algorithm of Lagrange polynom*)
 LP[1,i_,x_,x0_]:=1/(x0[[i+1]]-x0[[i]]) Det[({
- {f0[[i]], x0[[i]]-x},
- {f0[[i+1]], x0[[i+1]]-x}
+ {f[x0[[i]]], x0[[i]]-x},
+ {f[x0[[i+1]]], x0[[i+1]]-x}
 })];
 LP[n_,i_,x_,x0_]:=1/(x0[[i+n]]-x0[[i]]) Det[({
  {LP[n-1,i,x,x0], x0[[i]]-x},
@@ -36,18 +35,15 @@ LP[n_,i_,x_,x0_]:=1/(x0[[i+n]]-x0[[i]]) Det[({
 
 
 (*Newton polynom*)
-CNP[0,i_,x0_]:=f0[[i]];
-CNP[1,i_,x0_]:=1/(x0[[i+1]]-x0[[i]]) (f0[[i+1]] - f0[[i]]);
+CNP[0,i_,x0_]:=f[x0[[i]]];
+CNP[1,i_,x0_]:=1/(x0[[i+1]]-x0[[i]]) (f[x0[[i+1]]] - f[x0[[i]]]);
 CNP[n_,i_,x0_]:=1/(x0[[i+n]]-x0[[i]]) (CNP[n-1,i+1,x0] - CNP[n-1,i,x0]);
 NP[x_,x0_]:=Sum[CNP[j,1,x0]*Product[(x-x0[[k]]),{k,1,j}],{j,0,Length[x0]-1}];
 
 
-NP[#,x0]&/@x0
-f0
-
-
 (*Grid*)
-SG[x_,x0_,F_]:=Grid[Table[Table[F[n,i,x,x0],{n,1,Length[x0]-i}],{i,1,Length[x0]}]]//N;
+TSA[x_,x0_,F_]:=Grid[Table[Table[F[n,i,x,x0],{n,1,Length[x0]-i}],{i,1,Length[x0]}]]//N;
+TRO[x0_,F_]:=Grid[Table[Table[F[n,i,x0],{n,1,Length[x0]-i}],{i,1,Length[x0]}]]//N;
 
 
 "import complete"
--- Числ.мет.ЛР6.1.m
+++ Числ.мет.ЛР6.1.m
@@ -4,30 +4,26 @@ SetDirectory[NotebookDirectory[]]
 Import["\:0414\:0430\:043d\:043d\:044b\:0435.m"]
 
 
-P[#,x0]&/@p
+(*Result 3*)
+P[#,x0]&/@p-f[p]//Abs
 
 
-Abs[%-f[p]]
-
-
-LP[4,1,#1,x0]&/@p//N
-
-
-Abs[%-f[p]]
-
-
-SG[#,x0,LP]&/@p
-
-
-D[Sin[2x],x]
+(*Result 4*)
+LP[4,1,#1,x0]&/@p-f[p]//Abs
 M=MaxValue[Derivative[Length[x0]][f][x],x]
-
-
 R=M/5!*(pi/2)^5/2^9
 
 
+(*Result 5*)
+LP[4,1,#1,Tzero[5]]&/@p-f[p]//Abs
+
+
+(*Result 6*)
 NP[#,x0]&/@p-f[p]//Abs
-f[p]
+NP[#,Tzero[5]]&/@p-f[p]//Abs
 
 
+TRO[x0,CNP]
 
+
+TRO[Append[x0,#1],CNP]&/@p
